9d6b804081a557c56fa1ba97c476ab5718db3173
2354713722@qq.com <xmft2354>
2023-09-11 16:40:47 +0800

    重写日志模块.2

--- admin-api/internal/logger/logger.go
+++ admin-api/internal/logger/logger.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"path"
 	"sync"
 	"time"
 )
@@ -27,39 +26,94 @@ type ILogger interface {
 }
 
 type ToolLogger struct {
-	Prefix      string         // 日志前缀
-	TimeFormat  string         // 时间格式
-	filePath    string         // 日志文件路径
-	logLevel    LogLevel       // 默认日志等级
-	logChannel  chan *LogEntry // 写入日志的实体
-	isWriteFile bool           // 是否写文件
-	wg          sync.WaitGroup // 并发分组
-	out         io.Writer
-	fileSize    int64
-	maxFileSize int64
+	Prefix       string         // 日志前缀
+	TimeFormat   string         // 时间格式
+	filePath     string         // 日志文件路径
+	logLevel     LogLevel       // 默认日志等级
+	logChannel   chan *LogEntry // 写入日志的实体
+	isWriteFile  bool           // 是否写文件
+	consoleOut   io.Writer      // 控制台输出
+	consoleAsync bool           // 控制台输出是否异步
+	consoleBuff  *bytes.Buffer  // 控制台写buff
+	fileOut      io.Writer      // 文件输出
+	file         *os.File       // 文件句柄
+	fileBuff     *bytes.Buffer  // 文件写buff
+	maxFileSize  int64          // 日志文件阈值
+	wg           sync.WaitGroup // 并发分组
 }
 
 func DefaultLogger(logFile string) *ToolLogger {
-	logger := &ToolLogger{
-		filePath:    logFile,
-		logLevel:    Info,
-		logChannel:  make(chan *LogEntry, 10),
-		Prefix:      "Admin",
-		TimeFormat:  "2006-01-02 15:04:05",
-		maxFileSize: 10 * 1024 * 1024,
-		isWriteFile: true,
+	var (
+		logger *ToolLogger
+		file   *os.File
+		err    error
+	)
+	logger = &ToolLogger{
+		filePath:     logFile,
+		logLevel:     Info,
+		logChannel:   make(chan *LogEntry, 10),
+		Prefix:       "Admin",
+		TimeFormat:   "2006-01-02 15:04:05",
+		maxFileSize:  1 * 1024 * 1024,
+		isWriteFile:  true,
+		consoleAsync: false,
+		consoleBuff:  new(bytes.Buffer),
+		consoleOut:   os.Stdout,
 	}
-	file, err := openFile(buildPath(logFile))
-	if err != nil {
-		logger.isWriteFile = false
+	if file, err = openFile(buildPath(logFile)); err != nil {
 		PrintLogger(Error, "创建文件失败: %s", err.Error())
+		logger.isWriteFile = false
+	} else {
+		logger.file = file
+		logger.fileBuff = new(bytes.Buffer)
+		logger.fileOut = io.MultiWriter(file)
 	}
-	info, _ := file.Stat()
-	logger.fileSize = info.Size()
-	logger.out = io.MultiWriter(file, os.Stdout)
+	// 检测文件
+	logger.checkFile(file)
 	logger.wg.Add(1)
+	// 启动日志监听写入
 	go logger.processLog()
 	return logger
+}
+
+// checkFile 检测文件是否到达阈值
+func (l *ToolLogger) checkFile(file *os.File) {
+	var (
+		info        os.FileInfo
+		filePrefix  string
+		fileNewName string
+		logPath     string
+		count       int
+		err         error
+	)
+	if info, err = file.Stat(); err != nil {
+		PrintLogger(Error, "无法获取文件信息:%s", err.Error())
+		return
+	}
+	if info.Size() >= l.maxFileSize {
+		PrintLogger(Warn, "日志文件大小超过阈值")
+		if err := file.Close(); err != nil {
+			PrintLogger(Error, "关闭当前日志文件时出错:%s", err.Error())
+			return
+		}
+		filePrefix = fmt.Sprintf("%s_%s_", l.Prefix, time.Now().Format("20060102"))
+		if count, err = CountFileNum(l.filePath, filePrefix); err != nil {
+			PrintLogger(Error, "统计文件数量时出错: %s", err.Error())
+			return
+		}
+
+		fileNewName = buildPathWithName(l.filePath, fmt.Sprintf("%s%d.log", filePrefix, count+1))
+		logPath = buildPath(l.filePath)
+		if err = os.Rename(logPath, fileNewName); err != nil {
+			PrintLogger(Error, "重命名日志文件时出错: %s", err.Error())
+			return
+		}
+		if file, err = openFile(logPath); err != nil {
+			PrintLogger(Error, "创建新的日志文件时出错: %s", err.Error())
+			return
+		}
+		l.file = file
+	}
 }
 
 func (l *ToolLogger) GetPrefix() string {
@@ -72,77 +126,48 @@ func (l *ToolLogger) PrintLogger(level LogLevel, format string, args ...any) {
 
 // processLog 处理日志
 func (l *ToolLogger) processLog() {
-	buff := new(bytes.Buffer)
-	fileBuff := new(bytes.Buffer)
 	defer func() {
-		buff = nil
-		fileBuff = nil
+		if l.fileBuff != nil {
+			l.fileBuff = nil
+		}
+		l.consoleBuff = nil
 		_ = l.file.Close()
 		l.wg.Done()
 	}()
 	for entry := range l.logChannel {
 		// 写入文件
-		l.writeFile(entry, func() []byte {
-			defer fileBuff.Reset()
-			NotColor(fileBuff, l.Prefix, true)
-			NotColor(fileBuff, entry.Timestamp.Format(l.TimeFormat), true)
-			l.changeLevelNotColor(fileBuff, entry.Level)
-			fileBuff.WriteString(entry.Message)
-			fileBuff.WriteByte('\n')
-			return fileBuff.Bytes()
-		})
-		// 写入控制台
-		l.console(true, func() []byte {
-			defer buff.Reset()
-			Blue(buff, l.Prefix, true)
-			White(buff, entry.Timestamp.Format(l.TimeFormat), true)
-			l.changeLevel(buff, entry.Level)
-			buff.WriteString(entry.Message)
-			return buff.Bytes()
-		})
+		l.writeFile(entry)
+		// 异步写入控制台
+		if l.consoleAsync {
+			// 写入控制台
+			l.consolePrint(entry)
+		}
 	}
 }
 
 // writeFile 写文件
-func (l *ToolLogger) writeFile(entry *LogEntry, handler func() []byte) {
-	stat, _ := l.file.Stat()
-	if entry.Timestamp.Day() != time.Now().Day() {
-		_ = l.file.Close()
-		file, _ := openFile(path.Join(l.filePath, time.Now().Format("2006-01-02"), fmt.Sprintf("access_log_%03d.log", 1)))
-		l.fileIndex++
-		l.file = file
-	}
-	if stat.Size() >= l.maxFileSize {
-		fmt.Println("当前文件大小：", stat.Size(), l.maxFileSize)
-		// 分隔文件
-		_ = l.file.Sync()
-		_ = l.file.Close()
-
-		oldName := path.Join(l.filePath, time.Now().Format("2006-01-02"), fmt.Sprintf("access_log_%03d.log", l.fileIndex))
-		newName := path.Join(l.filePath, time.Now().Format("2006-01-02"), fmt.Sprintf("access_log_%03d.log", l.fileIndex+1))
-
-		if err := os.Rename(oldName, newName); err != nil {
-			PrintLogger(Error, "重命名失败: %s", err.Error())
-		}
-		file, err := openFile(newName)
-		if err != nil {
-			PrintLogger(Error, "创建文件失败: %s", err.Error())
-		}
-		l.fileIndex++
-		l.file = file
-	}
-	buffer := handler()
-	if _, err := l.file.Write(buffer); err != nil {
-		PrintLogger(Error, "写入文件失败: %s", err.Error())
+func (l *ToolLogger) writeFile(entry *LogEntry) {
+	l.checkFile(l.file)
+	defer l.fileBuff.Reset()
+	NotColor(l.fileBuff, l.Prefix, true)
+	NotColor(l.fileBuff, entry.Timestamp.Format(l.TimeFormat), true)
+	l.changeLevelNotColor(l.fileBuff, entry.Level)
+	l.fileBuff.WriteString(entry.Message)
+	l.fileBuff.WriteByte('\n')
+	if _, err := l.file.Write(l.fileBuff.Bytes()); err != nil {
+		panic(err.Error())
+		//PrintLogger(Error, "写入文件失败: %s", err.Error())
 	}
 }
 
-// console 写入控制台
-func (l *ToolLogger) console(isConsole bool, handler func() []byte) {
-	if !isConsole {
-		return
-	}
-	l.out.Write(handler())
+// consolePrint 控制台输出
+func (l *ToolLogger) consolePrint(entry *LogEntry) {
+	defer l.consoleBuff.Reset()
+	Blue(l.consoleBuff, l.Prefix, true)
+	White(l.consoleBuff, entry.Timestamp.Format(l.TimeFormat), true)
+	l.changeLevel(l.consoleBuff, entry.Level)
+	l.consoleBuff.WriteString(entry.Message)
+	_, _ = fmt.Fprintln(l.consoleOut, l.consoleBuff.String())
 }
 
 func (l *ToolLogger) changeLevel(buffer *bytes.Buffer, level LogLevel) {
@@ -183,7 +208,12 @@ func (l *ToolLogger) log(level LogLevel, namespace string, format string, args .
 	if level < l.logLevel {
 		return
 	}
-	l.logChannel <- NewEntry(level, namespace, fmt.Sprintf(format, args...))
+	entry := NewEntry(level, namespace, fmt.Sprintf(format, args...))
+	if !l.consoleAsync {
+		l.consolePrint(entry)
+	}
+	// 写入通道异步写入文件
+	l.logChannel <- entry
 }
 
 func (l *ToolLogger) Info(format string, args ...any) {
